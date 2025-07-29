d5c09c0810918ca81a319d4837b7f5bbe4e2eceb
seoyoung33 <bsy4357@gmail.com>
2025-07-29 15:42:56 +0900

    시험친거 수정

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,27 @@
 const work = document.querySelector('#work');
 const add = document.querySelector('#add');
 const list = document.querySelectorAll('li span');
-console.log(work, add, list);
+const ul = document.querySelector('.list')
+console.log(work, add, list, ul);
 add.addEventListener('click',()=>{
     if(work.value==""){
         alert('할 일을 입력하세요');
     }else{
         /* for(i=0; i<10; i++) */
-        list[0].textContent = work.value;
+        /* list[0].textContent = work.value; */
+        let li = document.createElement('li');
+        li.innerHTML = `<input type="checkbox" class="work_list">${work.value}</input><button type="button" class="del_btn">X</button>`
+        console.log(li);
+        work.value = '';
+        ul.appendChild(li);
+        /* 삭제버튼 */
+        const delBtn = document.querySelectorAll('.del_btn');
+        console.log(delBtn);
+        for(let i of delBtn){
+            i.addEventListener('click',()=>{
+                console.log(i);
+                i.parentNode.remove();
+            })
+        }
     }
 })
